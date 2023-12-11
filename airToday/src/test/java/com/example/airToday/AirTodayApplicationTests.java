9091667f96e7d64af3546ce79db76bdb87a1f7bd
Nub4Ever <rbocanu@yahoo.com>
2023-12-11 17:39:08 +0200

    Added SpringBoot

---
+++ airToday/src/test/java/com/example/airToday/AirTodayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airToday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirTodayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
